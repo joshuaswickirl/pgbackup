fc94917d776fc065597001ca0ed07d3e4b46bb58
Joshua Swick <joshuaswickirl@gmail.com>
2019-03-28 11:24:30 -0600

    Update --help text and prepped project for install

--- setup.py
+++ setup.py
@@ -11,6 +11,13 @@ setup(
     description='A utility for backing up PostgreSQL databases',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/joshuaswickirl/pgbackup'
-    packages=find_packages('src')
+    url='https://github.com/joshuaswickirl/pgbackup',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    install_requires=['boto3'],
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main'
+        ],
+    },
 )
